317bfe22629ce8563319421047f5cede3386a676
Ewuin <emguatemala@gmail.com>
2017-10-05 23:32:10 -0400

    changed file structure and urls

--- main/main/settings.py
+++ main/main/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'test_app',
+    'apps.test_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
 ]
 
 MIDDLEWARE = [
